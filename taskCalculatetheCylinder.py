4dab17640edc90dfb7a881cb6798427492987d58
sabasaid1 <130497837+sabasaid1@users.noreply.github.com>
2023-05-09 15:18:37 +0400

    Create taskCalculatetheCylinder.py

---
+++ taskCalculatetheCylinder.py
@@ -0,0 +1,16 @@
+#calculate the total of surface volume and area of cylinder
+
+
+r=6
+h=4
+PI = 3.14
+volume = PI*r**2*h
+print("volume", volume)
+
+Area= 2*PI*r*h+2*PI*r**2
+print("Area" ,format(Area,".2f"))
+
+
+
+
+
