e5a9f6cdcab61482ac1b5eef5e59289724d62a9f
sabasaid1 <130497837+sabasaid1@users.noreply.github.com>
2023-05-10 17:03:03 +0400

    Create TaskfileMemory.py

---
+++ TaskfileMemory.py
@@ -0,0 +1,11 @@
+#find file size
+
+x = int(input("enter memory :"))
+n= int(input("enter file num :"))
+y= int(input("enter file size :"))
+
+file = n*y
+if (x > file) :
+    print("yes")
+else :
+    print("no")
