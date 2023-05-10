3d234a8638483c4b33ff92b66be7ff074fc152b6
sabasaid1 <130497837+sabasaid1@users.noreply.github.com>
2023-05-10 17:06:19 +0400

    Create Taskfindloop.py

---
+++ Taskfindloop.py
@@ -0,0 +1,38 @@
+##Q1
+
+n=0
+while (n<=10) :
+    if (n % 2 == 0) :
+        print ("even",n)
+        n = n+2            
+    
+        
+##Q2
+        
+print("####")
+import math
+s=0
+while (s <= 10) :
+    print (s,s**2)
+    s = s + 1
+    
+##Q3
+    
+print("####")
+s=105
+while (s >= 7) :
+    print (s)
+    s = s - 7
+
+
+
+
+
+
+    
+        
+        
+        
+        
+
+    
