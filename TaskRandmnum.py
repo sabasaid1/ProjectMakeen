242efe37aa9a71a4ba132b0285e29b8d4c2b8a2a
sabasaid1 <130497837+sabasaid1@users.noreply.github.com>
2023-05-14 17:13:17 +0400

    Create TaskRandmnum.py

---
+++ TaskRandmnum.py
@@ -0,0 +1,36 @@
+'''m = "333-333"
+
+print(m.replace('-',''))'''
+
+import random
+
+from random import randint
+
+a = [1,12,3,6,4]
+
+#print(random.randrange(1,10))
+print(round(random.random()*10))'''
+
+'''
+c = randint(1,100)
+print(c)
+for i in range(5):
+    user = int(input("enter num:"))
+    if user<1 or user>100:
+        print("outside")
+    #else:
+    elif user == c:
+        print("you win")
+    elif user > c:
+        print("go lower")
+    else:
+        print(" go higher")
+          
+
+    
+    
+    
+    
+        
+        
+    
