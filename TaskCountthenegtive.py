24fe8fa45655659a6a824c5754a39f76bad78830
sabasaid1 <130497837+sabasaid1@users.noreply.github.com>
2023-05-15 17:36:19 +0400

    Create TaskCountthenegtive.py

---
+++ TaskCountthenegtive.py
@@ -0,0 +1,26 @@
+list1 = [22,3,-2,-1,10,1]
+
+
+c=0
+s=1
+for e in list1:
+    print(e, end=' ')
+    if e < 0:
+        c=c+1
+    s=e*s
+        
+print("\ncount",c)
+print("multi :",s)
+
+
+
+
+
+
+
+
+ 
+    
+    
+
+
